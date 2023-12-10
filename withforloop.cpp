18b1512e3304a06c8b3bf3c469efbeaff8e6e139
ugsakshii20 <138098129+ugsakshii20@users.noreply.github.com>
2023-12-10 15:59:13 +0530

    Update withforloop.cpp

--- withforloop.cpp
+++ withforloop.cpp
@@ -6,8 +6,10 @@ int main() {
    float num[] = {12, 76, 23, 9, 5};
    n = sizeof(num) / sizeof(num[0]);
    for(i = 0; i < n; i++)
+      {
    sum += num[i];
    avg = sum / n;
+      }
    cout<<"Average of all array elements is "<<avg;
    return 0;
 }
